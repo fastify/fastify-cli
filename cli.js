64209db7e97e460e5144fadcd2fc3dda3b63caeb
delvedor <tommydelved@gmail.com>
2016-12-03 11:32:05 +0100

    Added --pretty-logs options

--- cli.js
+++ cli.js
@@ -5,18 +5,19 @@
 const minimist = require('minimist')
 const Fastify = require('fastify')
 const path = require('path')
+const pino = require('pino')
+const pump = require('pump')
 const fs = require('fs')
-const help = fs.readFileSync(path.join(__dirname, 'help.txt'), 'utf8')
 
 function start (opts) {
   if (opts.help) {
-    console.log(help)
-    process.exit(0)
+    console.log(fs.readFileSync(path.join(__dirname, 'help.txt'), 'utf8'))
+    stop(0)
   }
 
   if (opts._.length !== 1) {
     console.log('Missing the file parameter')
-    process.exit(1)
+    stop(1)
   }
 
   runFastify(opts)
@@ -32,26 +33,39 @@ function runFastify (opts) {
     file = require(path.resolve(process.cwd(), opts._[0]))
   } catch (e) {
     console.log('Cannot find the specified file')
-    process.exit(1)
+    stop(1)
   }
 
-  const fastify = Fastify({
+  const options = {
     logger: {
       level: opts['log-level']
     }
-  })
+  }
+
+  if (opts['pretty-logs']) {
+    const pretty = pino.pretty()
+    pump(pretty, process.stdout, err => {
+      if (err) {
+        console.log(err)
+        stop(1)
+      }
+    })
+    options.logger.stream = pretty
+  }
+
+  const fastify = Fastify(options)
 
   fastify.register(file, function (err) {
     if (err) {
       console.log(err)
-      process.exit(1)
+      stop(1)
     }
   })
 
   fastify.listen(opts.port, function (err) {
     if (err) {
       console.log(err)
-      process.exit(1)
+      stop(1)
     }
     console.log(`Server listening on http://localhost:${fastify.server.address().port}`)
   })
