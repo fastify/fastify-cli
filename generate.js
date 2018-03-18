017dd8379b1327563f779373ff0d4469c109f5d9
Matteo Collina <hello@matteocollina.com>
2018-03-18 22:45:06 +0100

    The cli can now call the generator

--- generate.js
+++ generate.js
@@ -4,14 +4,20 @@ const {
   readFile,
   writeFile
 } = require('fs')
+const chalk = require('chalk')
 const path = require('path')
 const generify = require('generify')
 const minimist = require('minimist')
 const templatedir = path.join(__dirname, 'app_template')
 const cliPkg = require('./package')
 
-function generate (dir, cb) {
+function generate (dir, log, cb) {
+  if (!cb) {
+    cb = log
+    log = () => {}
+  }
   const pkgFile = path.join(dir, 'package.json')
+  log('info', `reading package.json in ${dir}`)
   readFile(pkgFile, (err, data) => {
     if (err) {
       return cb(err)
@@ -40,25 +46,61 @@ function generate (dir, cb) {
       'tap': cliPkg.devDependencies['tap']
     })
 
+    log('debug', `edited package.json, saving`)
+
     writeFile(pkgFile, JSON.stringify(pkg), (err) => {
       if (err) {
         return cb(err)
       }
 
-      generify(templatedir, dir, {}, (err) => {
+      log('debug', `saved package.json`)
+      log('info', `copying sample project`)
+
+      generify(templatedir, dir, {}, function (file) {
+        log('debug', `generated ${file}`)
+      }, function (err) {
         if (err) {
           return cb(err)
         }
 
+        log('info', `project ${pkg.name} generated successfully`)
+        log('debug', `run '${chalk.bold('npm install')}' to install the dependencies`)
+        log('debug', `run '${chalk.bold('npm start')}' to start the application`)
+        log('debug', `run '${chalk.bold('npm test')}' to execute the unit tests`)
         cb()
       })
     })
   })
 }
 
+const levels = {
+  'debug': 0,
+  'info': 1,
+  'error': 2
+}
+
+const colors = [
+  (l) => l,
+  chalk.green,
+  chalk.red
+]
+
 function cli (args) {
-  generate(minimist(args, {
-  }))
+  const opts = minimist(args)
+  generate(opts._[0] || process.cwd(), log, function (err) {
+    if (err) {
+      log('error', err.message)
+      process.exit(1)
+    }
+  })
+
+  function log (severity, line) {
+    const level = levels[severity] || 0
+    if (level === 1) {
+      line = '--> ' + line
+    }
+    console.log(colors[level](line))
+  }
 }
 
 module.exports = {
