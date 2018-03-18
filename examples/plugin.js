67cf504cd07388b4873e33da37d1251167bea342
Davide Tantillo <davide.tantillo@makeitapp.eu>
2018-03-18 14:36:20 +0100

    Add unit test for body-limit option

--- examples/plugin.js
+++ examples/plugin.js
@@ -4,5 +4,8 @@ module.exports = function (fastify, options, next) {
   fastify.get('/', function (req, reply) {
     reply.send({ hello: 'world' })
   })
+  fastify.post('/', function (req, reply) {
+    reply.send({ hello: 'world' })
+  })
   next()
 }
