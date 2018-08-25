06eac3b7147e2b1a9242ee78b5fd85e4bfa06054
Matteo Collina <matteo.collina@gmail.com>
2018-08-25 16:44:40 +0200

    Pass options down to the autoloaded plugins (#62)

--- app_template/app.js
+++ app_template/app.js
@@ -12,13 +12,15 @@ module.exports = function (fastify, opts, next) {
   // those should be support plugins that are reused
   // through your application
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins')
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
   })
 
   // This loads all plugins defined in services
   // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'services')
+    dir: path.join(__dirname, 'services'),
+    options: Object.assign({}, opts)
   })
 
   // Make sure to call next when done
