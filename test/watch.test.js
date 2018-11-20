9f03c165ccfe7928338e46c8425b43bb83a7d36a
gaogao <gaogao1130@gmail.com>
2018-11-20 17:45:38 +0800

    Add ignore-watch env variable to ignore changes to the specified file or directory under the process.cwd() directory (#126)
    * Add ignore-watch env variable to ignore changes to the specified file or directory under the process.cwd() directory
    * change about --ignore-watch command illustration in readme.md
    * rename constant: IGNORE_ARRAY to IGNORE_WATCH
    * move and rename lib/watch/test/utils.test.js to test/watch.test.js
    * remove lib/watch/test/**/*.test.js unit test in package.json
    * add use strict at the top

---
+++ test/watch.test.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const { arrayToRegExp } = require('../lib/watch/utils')
+const { IGNORE_WATCH } = require('../lib/watch/constants')
+
+const t = require('tap')
+const test = t.test
+
+test('should equal expect RegExp', t => {
+  t.plan(1)
+
+  const expectRegExp = /(node_modules|build|dist|\.git|bower_components|logs)/
+  const regExp = arrayToRegExp(IGNORE_WATCH)
+
+  t.deepEqual(regExp, expectRegExp)
+})
