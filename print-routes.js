4d7ae7dc824e1d32b9a407afcaf981340e279ff4
Francis Brito <fr.br94@gmail.com>
2019-08-11 16:43:19 -0400

    refactor(plugin): abstract out common logic in start and print routes commands

--- print-routes.js
+++ print-routes.js
@@ -5,26 +5,23 @@
 require('dotenv').config()
 
 const assert = require('assert')
-const path = require('path')
-const fs = require('fs')
-const resolveFrom = require('resolve-from')
 const parseArgs = require('./args')
 const log = require('./log')
+const { exit, requireFastifyForModule, requireServerPluginFromPath, showHelpForCommand } = require('./util')
 
 let Fastify = null
 
 function loadModules (opts) {
   try {
-    const basedir = path.resolve(process.cwd(), opts._[0])
-
-    Fastify = require(resolveFrom.silent(basedir, 'fastify') || 'fastify')
+    Fastify = requireFastifyForModule(opts._[0]).module
   } catch (e) {
     module.exports.stop(e)
   }
 }
 
 function showHelp () {
-  console.log(fs.readFileSync(path.join(__dirname, 'help', 'print-routes.txt'), 'utf8'))
+  showHelpForCommand('print-routes')
+
   return module.exports.stop()
 }
 
@@ -48,28 +45,14 @@ function printRoutes (args, cb) {
 }
 
 function runFastify (opts, cb) {
-  const filePath = path.resolve(process.cwd(), opts._[0])
   cb = cb || assert.ifError
-
-  if (!fs.existsSync(filePath)) {
-    return module.exports.stop(`${opts._[0]} doesn't exist within ${process.cwd()}`)
-  }
 
   let file = null
 
   try {
-    file = require(filePath)
+    file = requireServerPluginFromPath(opts._[0])
   } catch (e) {
     return module.exports.stop(e)
-  }
-
-  if (file.length !== 3 && file.constructor.name === 'Function') {
-    return module.exports.stop(new Error('Plugin function should contain 3 arguments. Refer to ' +
-      'documentation for more information.'))
-  }
-  if (file.length !== 2 && file.constructor.name === 'AsyncFunction') {
-    return module.exports.stop(new Error('Async/Await plugin function should contain 2 arguments.' +
-      'Refer to documentation for more information.'))
   }
 
   const fastify = Fastify(opts.options)
@@ -95,18 +78,11 @@ function runFastify (opts, cb) {
 }
 
 function stop (message) {
-  if (message instanceof Error) {
-    console.log(message)
-    process.exit(1)
-  } else if (message) {
-    console.log(`Warn: ${message}`)
-    process.exit(1)
-  }
-  process.exit()
+  exit(message)
 }
 
 function cli (args) {
-  printRoutes(args)
+  printRoutes(args).close()
 }
 
 module.exports = { cli, stop, printRoutes }
