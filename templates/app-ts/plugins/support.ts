77375713479a626eddf166ccaf76645dd456e647
Peter Balazs <frikille@gmail.com>
2020-04-28 15:33:38 +0100

    update fastify to v3

--- templates/app-ts/plugins/support.ts
+++ templates/app-ts/plugins/support.ts
@@ -1,11 +1,9 @@
 import fp from 'fastify-plugin'
-import * as http from 'http'
-import fastify from 'fastify'
+import fastify, { FastifyInstance } from 'fastify'
 
 // the use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
-
-export default fp(async function (fastify: fastify.FastifyInstance, opts: any) {
+export default fp(async function (fastify: FastifyInstance, opts: any) {
   fastify.decorate('someSupport', function () {
     return 'hugs'
   })
@@ -14,11 +12,7 @@ export default fp(async function (fastify: fastify.FastifyInstance, opts: any) {
 // Using Typescript you need to extend FastifyInstance
 // type declaration with your plugin
 declare module 'fastify' {
-  export interface FastifyInstance<
-  HttpServer = http.Server,
-  HttpRequest = http.IncomingMessage,
-  HttpResponse = http.ServerResponse
-> {
+  export interface FastifyInstance {
     someSupport(): string;
   }
 }
