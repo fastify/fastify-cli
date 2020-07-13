5bf30627cae78ae1537fe95661c4ffd6ec8c4f1d
MacThanos <phongc@gmail.com>
2020-07-13 10:07:18 +1000

    Update root.js
    Only have one async function

--- templates/app/routes/root.js
+++ templates/app/routes/root.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = async function (fastify, opts) {
+module.exports = function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return { root: true }
   })
