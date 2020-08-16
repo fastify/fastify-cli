370cda79262e422f6af8db18e1cf02d0207b0c53
Denys Otrishko <shishugi@gmail.com>
2020-08-16 19:50:10 +0300

    Multiple improvements to TS template
    * use strict: true in tsconfig.json
    * include tsc in `npm test` to check types before running tests
    * use ES2018 as TS target
    * update code to consistently use single quotes and have capitalized
      comments
    * add @types/tap and fix typings for tests
    * change exports for plugins and routes to use correct types with
      type inference
    * consistently use/ignore promises returned by fastify functions
    * move all source to src/ folder
    * create separate tsc target for test folder and run it before tap

--- templates/app-ts/routes/example/index.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from "fastify"
-
-export default async function (fastify: FastifyInstance, opts: any) {
-  fastify.get('/', async function (request, reply) {
-    return 'this is an example'
-  })
-}
--- templates/app-ts/test/routes/root.test.ts
+++ templates/app-ts/test/routes/root.test.ts
@@ -2,7 +2,7 @@ import { test } from 'tap'
 import { build } from '../helper'
 
 test('default root route', async (t) => {
-  const app = build(t)
+  const app = await build(t)
 
   const res = await app.inject({
     url: '/'
