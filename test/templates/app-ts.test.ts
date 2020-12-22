c33b9d24c5ae39806e89076fa84bd17762d525dc
Igor Savin <iselwin@gmail.com>
2020-12-22 18:18:31 +0200

    Add test for starting app from TS template

---
+++ test/templates/app-ts.test.ts
@@ -0,0 +1,36 @@
+import { fastify } from 'fastify'
+import { test } from 'tap'
+const sgetOriginal = require('simple-get').concat
+
+import { app } from '../../templates/app-ts/src/app'
+
+const PORT = 3001;
+const sget = (opts: Record<string, any>): Record<string, any> => {
+    return new Promise((resolve, reject) => {
+        sgetOriginal(opts, (err: Error, response: any, body: any) => {
+            if (err) return reject(err)
+            return resolve({ response, body })
+        })
+    })
+}
+
+test('should print routes for TS app', async t => {
+    t.plan(4)
+
+    const fastifyApp = fastify({}, );
+    await app(fastifyApp, {});
+    await fastifyApp.ready();
+    await fastifyApp.listen(PORT)
+
+    const { response, body } = await sget({
+        method: 'GET',
+        url: `http://localhost:${PORT}`
+    })
+    t.strictEqual(response.statusCode, 200)
+    t.strictEqual(response.headers['content-length'], '' + body.length)
+    t.deepEqual(JSON.parse(body), { root: true })
+
+    await fastifyApp.close();
+    t.pass('server closed')
+})
+
