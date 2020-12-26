15bcfee25788889609d3a9909f52db7022747504
Matteo Collina <hello@matteocollina.com>
2020-12-26 19:26:06 +0100

    Fix eject and eject-ts tests

--- eject-ts.js
+++ eject-ts.js
@@ -4,27 +4,27 @@ const path = require('path')
 const generify = require('generify')
 const log = require('./log')
 
-function eject () {
+function eject (dir) {
   return new Promise((resolve, reject) => {
     generify(
       path.join(__dirname, 'templates', 'eject-ts'),
-      '.',
+      dir,
       {},
       function (file) {
         log('debug', `generated ${file}`)
-        resolve()
       },
       function (err) {
         if (err) {
           return reject(err)
         }
+        resolve()
       }
     )
   })
 }
 
 function cli () {
-  eject().catch(function (err) {
+  eject(process.cwd()).catch(function (err) {
     if (err) {
       log('error', err.message)
       process.exit(1)
