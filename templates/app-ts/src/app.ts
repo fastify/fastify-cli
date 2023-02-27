6d975c1ee682ec74f72d5eef0b2dcde11b4681c8
Dien Vu <meotimdihia@gmail.com>
2023-02-27 17:37:30 +0700

    Improve the Typescript template.

--- templates/app-ts/src/app.ts
+++ templates/app-ts/src/app.ts
@@ -2,8 +2,9 @@ import { join } from 'path';
 import AutoLoad, {AutoloadPluginOptions} from '@fastify/autoload';
 import { FastifyPluginAsync, FastifyServerOptions } from 'fastify';
 
-export type AppOptions = FastifyServerOptions & Partial<AutoloadPluginOptions>;
+export interface AppOptions extends FastifyServerOptions, Partial<AutoloadPluginOptions> {
 
+}
 // Pass --options via CLI arguments in command to enable these options.
 const options: AppOptions = {
 }
