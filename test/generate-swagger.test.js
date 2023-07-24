0e476fcef299660dd60ded4d285b0bec53f889ff
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-07-24 12:56:46 +0200

    refactor: remove ts swagger generate

--- test/generate-swagger.test.js
+++ test/generate-swagger.test.js
@@ -5,23 +5,12 @@ const { generateSwagger } = require('../generate-swagger')
 
 const swaggerplugindir = path.join(__dirname, 'swaggerplugindir')
 const swaggerplugin = path.join(swaggerplugindir, 'plugin.js')
-const swaggerpluginTs = path.join(swaggerplugindir, 'plugin.ts')
 
 test('should generate swagger', async (t) => {
   t.plan(1)
 
   try {
     const swagger = JSON.parse(await generateSwagger([swaggerplugin]))
-    t.equal(swagger.openapi, '3.0.3')
-  } catch (err) {
-    t.error(err)
-  }
-})
-test('should generate swagger with ts file', async (t) => {
-  t.plan(1)
-
-  try {
-    const swagger = JSON.parse(await generateSwagger([swaggerpluginTs]))
     t.equal(swagger.openapi, '3.0.3')
   } catch (err) {
     t.error(err)
