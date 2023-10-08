f8e5e3a4844f1ec63817c69f46ed5faec9994538
Sumeet Haryani <sumeetharyani3@gmail.com>
2023-10-08 22:44:16 +0530

    Add fix to access the plugin inside tests

--- templates/app/test/helper.js
+++ templates/app/test/helper.js
@@ -1,35 +1,35 @@
-'use strict'
+"use strict";
 
 // This file contains code that we reuse
 // between our tests.
 
-const { build: buildApplication } = require('fastify-cli/helper')
-const path = require('path')
-const AppPath = path.join(__dirname, '..', 'app.js')
-
+const { build: buildApplication } = require("fastify-cli/helper");
+const path = require("path");
+const AppPath = path.join(__dirname, "..", "app.js");
+const fp = require("fastify-plugin");
 // Fill in this config with all the configurations
 // needed for testing the application
-function config () {
-  return {}
+function config() {
+  return {};
 }
 
 // automatically build and tear down our instance
-async function build (t) {
+const build = fp(async function (t) {
   // you can set all the options supported by the fastify CLI command
-  const argv = [AppPath]
+  const argv = [AppPath];
 
   // fastify-plugin ensures that all decorators
   // are exposed for testing purposes, this is
   // different from the production setup
-  const app = await buildApplication(argv, config())
+  const app = await buildApplication(argv, config());
 
   // tear down our app after we are done
-  t.teardown(app.close.bind(app))
+  t.teardown(app.close.bind(app));
 
-  return app
-}
+  return app;
+});
 
 module.exports = {
   config,
-  build
-}
+  build,
+};
