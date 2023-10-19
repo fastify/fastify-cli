e2a6f6280ba64c40101298a16f4b278bd35786e9
Frazer Smith <frazer.dev@outlook.com>
2023-10-19 10:45:51 +0100

    chore: add missing `use strict` directives (#673)

--- templates/plugin/test/index.test.js
+++ templates/plugin/test/index.test.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const { test } = require('tap')
 
 test('should register the correct decorator', async t => {
--- test/generate-swagger.test.js
+++ test/generate-swagger.test.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const path = require('path')
 const t = require('tap')
 const { test } = t
