3406ae218fa47e8a8a7579fad4e52407b4f91f7f
Aranđel Šarenac <11753867+big-kahuna-burger@users.noreply.github.com>
2023-11-28 10:34:55 +0100

    ci: fix args expansion (#5)

--- suite-runner.js
+++ suite-runner.js
@@ -5,13 +5,14 @@ const glob = require('glob')
 
 const pattern = process.argv[process.argv.length - 1]
 
+console.info(`Running tests matching ${pattern}`)
+
 glob(pattern, (err, matches) => {
   if (err) {
     console.error(err)
     process.exit(1)
   }
   const resolved = matches.map(file => path.resolve(file))
-  run({ files: resolved })
-    .compose(spec)
-    .pipe(process.stdout)
+  const testRs = run({ files: resolved }).compose(spec)
+  testRs.pipe(process.stdout)
 })
