5420275a66ebac076616523a6616633cc8304af7
big-kahuna-burger <arandjel.sarenac@gmail.com>
2023-11-28 08:41:16 +0100

    ci: run templates on 20 only

--- should-test-suite.js
+++ should-test-suite.js
@@ -2,7 +2,7 @@ const suite = process.argv[2]
 
 function shouldTest(suiteName) {
   const nodeMajorVersion = process.versions.node.split('.').map(x => parseInt(x, 10))[0]
-  const code = nodeMajorVersion < 18 ? 0 : 1
+  const code = nodeMajorVersion < 20 ? 0 : 1
   if (!code) {
     console.info(`Skipped template test suite for ${suiteName} on node ${nodeMajorVersion}`)
   }
