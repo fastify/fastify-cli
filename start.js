5823f63c19a3b9dfd17289e910c7891edb4f0da0
Ahmed Mohsen <ahmed.yalahwy@gmail.com>
2024-01-02 17:55:20 +0200

    Update start.js
    Signed-off-by: Ahmed Mohsen <ahmed.yalahwy@gmail.com>

--- start.js
+++ start.js
@@ -135,7 +135,7 @@ async function runFastify (args, additionalOptions, serverOptions) {
     options = deepmerge(options, serverOptions)
   }
 
-  if (opts.options && file.options) {
+  if (opts.options || file.options) {
     options = deepmerge(options, file.options)
   }
 
