24aeed12b2056d7dd8141c8df74bdaea921e7eef
Phạm Thành Trung <adamward459@gmail.com>
2024-02-23 16:34:42 +0700

    Fix test script in generate-typescript.test.js

--- test/generate-typescript.test.js
+++ test/generate-typescript.test.js
@@ -126,7 +126,7 @@ function define (t) {
         // by default this will be ISC but since we have a MIT licensed pkg file in upper dir, npm will set the license to MIT in this case
         // so for local tests we need to accept MIT as well
         t.ok(pkg.license === 'ISC' || pkg.license === 'MIT')
-        t.equal(pkg.scripts.test, 'npm run build:ts && tsc -p test/tsconfig.json && c8 node --test -r ts-node/register test/**/*.ts')
+        t.equal(pkg.scripts.test, 'npm run build:ts && tsc -p test/tsconfig.json && c8 node --test -r ts-node/register \\"test/**/*.ts\\"')
         t.equal(pkg.scripts.start, 'npm run build:ts && fastify start -l info dist/app.js')
         t.equal(pkg.scripts['build:ts'], 'tsc')
         t.equal(pkg.scripts['watch:ts'], 'tsc -w')
