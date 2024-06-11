83c9ca26edc39be216ab263a57a4933eccb79c22
Jean <110341611+jean-michelet@users.noreply.github.com>
2024-06-11 15:29:03 +0200

    refactor: uninstall global listeners not needed on eject templates (#732)

--- templates/eject-esm/server.js
+++ templates/eject-esm/server.js
@@ -22,16 +22,11 @@ const app = Fastify({
 app.register(appService)
 
 // delay is the number of milliseconds for the graceful close to finish
-const closeListeners = closeWithGrace({ delay: process.env.FASTIFY_CLOSE_GRACE_DELAY || 500 }, async function ({ signal, err, manual }) {
+closeWithGrace({ delay: process.env.FASTIFY_CLOSE_GRACE_DELAY || 500 }, async function ({ signal, err, manual }) {
   if (err) {
     app.log.error(err)
   }
   await app.close()
-})
-
-app.addHook('onClose', (instance, done) => {
-  closeListeners.uninstall()
-  done()
 })
 
 // Start listening.
