2de39f5eabc991fb27ebb4c28e3dc163310a73f4
Frazer Smith <frazer.dev@icloud.com>
2024-12-16 12:40:05 +0000

    chore: rebase

--- templates/app-ts/test/helper.ts
+++ templates/app-ts/test/helper.ts
@@ -1,7 +1,7 @@
 // This file contains code that we reuse between our tests.
-const helper = require('fastify-cli/helper.js')
 import * as path from 'node:path'
 import * as test from 'node:test'
+const helper = require('fastify-cli/helper.js')
 
 export type TestContext = {
   after: typeof test.after
