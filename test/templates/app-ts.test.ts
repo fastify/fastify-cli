409c06230f35508b1b55ccfe26cc8209cadf9c88
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-07-12 00:12:58 +0200

    chore: removed simple get from start test

--- test/templates/app-ts.test.ts
+++ test/templates/app-ts.test.ts
@@ -3,16 +3,6 @@ import { test, TestContext } from 'node:test'
 
 import { AddressInfo } from 'node:net'
 import appDefault, { app } from '../../templates/app-ts/src/app'
-const sgetOriginal = require('simple-get').concat
-
-const sget = (opts: Record<string, any>): Record<string, any> => {
-  return new Promise((resolve, reject) => {
-    sgetOriginal(opts, (err: Error, response: any, body: any) => {
-      if (err) return reject(err)
-      return resolve({ response, body })
-    })
-  })
-}
 
 test('should print routes for TS app', async (t: TestContext) => {
   t.plan(4)
@@ -22,12 +12,11 @@ test('should print routes for TS app', async (t: TestContext) => {
   await fastifyApp.ready()
   await fastifyApp.listen({ port: 3000 })
 
-  const { response, body } = await sget({
-    method: 'GET',
-    url: `http://localhost:${(fastifyApp.server.address() as AddressInfo).port}`
-  })
-  t.assert.equal(response.statusCode, 200)
-  t.assert.equal(response.headers['content-length'], '' + body.length)
+  const result = await fetch(`http://localhost:${(fastifyApp.server.address() as AddressInfo).port}`)
+  t.assert.equal(result.status, 200)
+
+  const body = await result.text()
+  t.assert.equal(result.headers.get('content-length'), '' + body.length)
   t.assert.deepStrictEqual(JSON.parse(body), { root: true })
 
   await fastifyApp.close()
@@ -42,12 +31,11 @@ test('should print routes for default TS app', async (t: TestContext) => {
   await fastifyApp.ready()
   await fastifyApp.listen({ port: 3000 })
 
-  const { response, body } = await sget({
-    method: 'GET',
-    url: `http://localhost:${(fastifyApp.server.address() as AddressInfo).port}`
-  })
-  t.assert.equal(response.statusCode, 200)
-  t.assert.equal(response.headers['content-length'], '' + body.length)
+  const result = await fetch(`http://localhost:${(fastifyApp.server.address() as AddressInfo).port}`)
+  t.assert.equal(result.status, 200)
+
+  const body = await result.text()
+  t.assert.equal(result.headers.get('content-length'), '' + body.length)
   t.assert.deepStrictEqual(JSON.parse(body), { root: true })
 
   await fastifyApp.close()
