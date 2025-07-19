ea9ec29711bdb48fe2df26debf9ac3e463ace09b
Tony133 <a.tripodi133@gmail.com>
2025-07-19 12:54:33 +0200

    chore: update test

--- test/graceful-shutdown.test.js
+++ test/graceful-shutdown.test.js
@@ -1,8 +1,7 @@
 'use strict'
 
-const { test } = require('node:test')
-// Tests skip on win32 platforms due SIGINT signal is not supported across all windows platforms
-const testWin = (process.platform === 'win32') ? test.skip : test
+const { test, beforeEach, afterEach } = require('node:test')
+const assert = require('assert')
 const sinon = require('sinon')
 const start = require('../start')
 
@@ -11,12 +10,16 @@ function getPort () {
   return '' + _port++
 }
 
-let spy = null
 let fastify = null
+let spy = null
 let signalCounter = null
 const sandbox = sinon.createSandbox()
 
-test.beforeEach(async () => {
+// Skip tests on Windows
+const isWindows = process.platform === 'win32'
+const conditionalTest = isWindows ? test.skip : test
+
+beforeEach(async () => {
   signalCounter = process.listenerCount('SIGINT')
 
   const argv = ['-p', getPort(), './examples/plugin.js']
@@ -24,28 +27,28 @@ test.beforeEach(async () => {
   spy = sinon.spy(fastify, 'close')
 })
 
-test.afterEach(async () => {
+afterEach(async () => {
   sandbox.restore()
 })
 
-test('should add and remove SIGINT listener as expected', async (t) => {
-  t.plan(2)
-
-  t.assert.strictEqual(process.listenerCount('SIGINT'), signalCounter + 1)
+conditionalTest('should add and remove SIGINT listener as expected', async (t) => {
+  assert.strictEqual(process.listenerCount('SIGINT'), signalCounter + 1)
 
   await fastify.close()
 
-  t.assert.strictEqual(process.listenerCount('SIGINT'), signalCounter)
+  assert.strictEqual(process.listenerCount('SIGINT'), signalCounter)
 })
 
-test('should have called fastify.close() when receives a SIGINT signal', async t => {
-  process.once('SIGINT', () => {
-    sinon.assert.called(spy)
+conditionalTest('should call fastify.close() on SIGINT', async (t) => {
+  const sigintHandler = () => {
+    try {
+      sinon.assert.called(spy)
+      t.pass('fastify.close() was called on SIGINT')
+    } finally {
+      process.exit() // Clean exit
+    }
+  }
 
-    t.assert.ok('SIGINT signal handler called')
-
-    process.exit()
-  })
-
+  process.once('SIGINT', sigintHandler)
   process.kill(process.pid, 'SIGINT')
 })
