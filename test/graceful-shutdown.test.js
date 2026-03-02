e6e6bd60d0b3afd7813524bc59225fb2f6e2e11f
Matteo Collina <hello@matteocollina.com>
2026-03-02 17:00:41 +0100

    test: migrate tap suite to node:test

--- test/graceful-shutdown.test.js
+++ test/graceful-shutdown.test.js
@@ -1,8 +1,10 @@
 'use strict'
 
-const t = require('tap')
+const { once } = require('node:events')
+const { test, beforeEach, afterEach } = require('node:test')
+const assert = require('node:assert/strict')
 // Tests skip on win32 platforms due SIGINT signal is not supported across all windows platforms
-const test = (process.platform === 'win32') ? t.skip : t.test
+const testFn = (process.platform === 'win32') ? (name, fn) => test(name, { skip: true }, fn) : test
 const sinon = require('sinon')
 const start = require('../start')
 
@@ -17,7 +19,7 @@ let fastify = null
 let signalCounter = null
 const sandbox = sinon.createSandbox()
 
-t.beforeEach(async () => {
+beforeEach(async () => {
   signalCounter = process.listenerCount('SIGINT')
 
   const argv = ['-p', getPort(), './examples/plugin.js']
@@ -25,30 +27,30 @@ t.beforeEach(async () => {
   spy = sinon.spy(fastify, 'close')
 })
 
-t.afterEach(async () => {
+afterEach(async () => {
   sandbox.restore()
 })
 
-test('should add and remove SIGINT listener as expected ', async t => {
-  t.plan(2)
-
-  t.equal(process.listenerCount('SIGINT'), signalCounter + 1)
+testFn('should add and remove SIGINT listener as expected ', async () => {
+  assert.equal(process.listenerCount('SIGINT'), signalCounter + 1)
 
   await fastify.close()
 
-  t.equal(process.listenerCount('SIGINT'), signalCounter)
-
-  t.end()
+  assert.equal(process.listenerCount('SIGINT'), signalCounter)
 })
 
-test('should have called fastify.close() when receives a SIGINT signal', async t => {
-  process.once('SIGINT', () => {
-    sinon.assert.called(spy)
+testFn('should have called fastify.close() when receives a SIGINT signal', async (t) => {
+  const exit = process.exit
+  process.exit = sinon.spy()
 
-    t.end()
-
-    process.exit()
+  t.after(() => {
+    process.exit = exit
   })
 
+  const sigintPromise = once(process, 'SIGINT')
+
   process.kill(process.pid, 'SIGINT')
+  await sigintPromise
+
+  sinon.assert.called(spy)
 })
